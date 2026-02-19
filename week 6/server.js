9705ec50a53fe9c72a737f285b1d9c49bd4fbfec
milesc0628 <miles.ec888@gmail.com>
2026-02-19 17:05:25 -0500

    added person and updated server js

--- week 6/server.js
+++ week 6/server.js
@@ -10,7 +10,7 @@ const port = 3000;
 // handlebars is a view engine for express. most up to date
 const hbs = require("express-handlebars");
 
-app.engine("handlebars", hbs.engine);
+app.engine("handlebars", hbs.engine());
 app.set("view engine", "handlebars");
 
 // the path module is used to work with file and directory paths
@@ -60,12 +60,30 @@ app.delete("/api/items/:id", (req, res) => {
 });
 
 app.get("/directory", (req,res) => {
-    console.log("he");
     res.render("directory", { people: directory });
 });
 
+app.get("/person/add", (req,res) => {
+    console.log(req.params);
+    console.log(req.query);
+    directory.push({
+        id: parseInt(req.query.id),
+        first_name: req.query.first_name,
+        last_name: req.query.last_name,
+        email: req.query.email,
+        address: req.query.address,
+        city: req.query.city,
+        state: req.query.state,
+        zip: req.query.zip,
+    });
+    res.send("add-person");
+});
+
 app.get("/directory/:id", (req,res) => {
-    res.render("directory", { people: directory });
+    const id = req.params.id;
+    console.log(id);
+    let person = directory.find((p) => p.id == id);
+    res.render("person", { person: person, title: person.first_name + "" + person.last_name});
 });
 
 app.listen(port, () => {
